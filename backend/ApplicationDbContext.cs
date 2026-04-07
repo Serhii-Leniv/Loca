6490f603d3c41c852279baf4391cdd81fc771e2d
Stanislav Fedorniak <stanislav.fedorniak@gmail.com>
2026-04-07 11:45:43 +0300

    feat: configure database infrastructure and user model

---
+++ backend/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+namespace Loca.Backend.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Users> Users { get; set; } = null!;
+
+
+    }
+}
